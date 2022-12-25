437bfc44196f0977536376892ffdb4b59a0a6c84
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2022-12-24 18:00:38 -0600

    Estudio de optionals
    Avancé con 2 videos del curso, estudiando las variables tipo optional y los unwrap necesarios al usar estas variables.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     /* Para conectar múltiples botones a un mismo IBAction, dejamos presionada la tecla control normal, y la movemos al bloque de código de la IBAction, o simplemente arrastramos desde el círculo de la IBAction al botón que queremos agregar, sin presionar ninguna tecla. */
     @IBAction func hardnessSelected(_ sender: UIButton) {
         // Imprimimos el título del botón que fue presionado: print(sender.currentTitle!)
-        let hardness = sender.currentTitle
+        let hardness = sender.currentTitle!
         // Imprimimos el valor del tiempo de cada botón dependiendo de cuál fue presionado con una estructura if-else
         if hardness == "Soft" {
             print(softTime)
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
     
     // Ahora creamos la misma función para los botones, pero usando el diccionario y una estructura switch.
     @IBAction func hardnessButton(_ sender: UIButton) {
-        let hardness = sender.currentTitle
+        let hardness = sender.currentTitle!
         switch(hardness) {
             case "Soft":
                 print(eggTimes["Soft"]!)
@@ -40,8 +40,14 @@ class ViewController: UIViewController {
                 print("Error")
         }
     }
+    
+    // La solución más eficiente, propuesta por Angela.
+    @IBAction func angelaSolution(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        print(eggTimes[hardness]!) // Cuando obtenemos el valor de una llave, obtenemos un valor optional.
+    }
+    
 }
-
 
 /* Notas:
     Hay una advertencia que aparece cuando creamos una variable que no utilizamos para nada, y nos sugiere cambiarla por un guión bajo _ para que nosotros, y otras personas, entendamos el código más rápidamente. Esto es especialmente útil en estructuras repetitivas en donde no utilizamos el valor de la variable iterativa.
